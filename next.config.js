9c9b52520a93d08dc73ace8e60ad2514b2c31b7a
mynolog <minholee.log@gmail.com>
2025-01-18 13:40:35 +0900

    🚧 chore: next 환경 설정에 images.remotePatterns 추가

--- next.config.js
+++ next.config.js
@@ -2,4 +2,14 @@ module.exports = {
   experimental: {
     ppr: true,
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'qctxtzxpbyfftroayoaq.supabase.co',
+        pathname: '/storage/v1/object/**',
+      },
+    ],
+  },
 }
